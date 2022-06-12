6814616238676ebce6f18f5ac53c5853a044ffcc
Nhat2807999 <nhat28071999@gmail.com>
2022-06-12 22:56:27 +0700

    buid done home page

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React , {useCallback, useState , useEffect} from 'react'
 import PropTypes from 'prop-types'
 import {Link} from 'react-router-dom'
+
+import Button from './Button'
+
 const HeroSlider = props => {
   const data = props.data;
-  const activeSlide = 0; 
+
+  const timeout = props.timeOut ? props.timeOut : 3000;
+
+  const [activeSlide,setActiveSlide] = useState(0);
+
+  const nextSlider = useCallback(
+    () => {
+        const index = activeSlide + 1 === data.length ? 0 : activeSlide + 1;
+        setActiveSlide(index)
+     },[activeSlide,data]
+  )
+
+ const prevSlider = () => {
+    const index = activeSlide - 1  < 0  ? data.length - 1  : activeSlide - 1;
+    setActiveSlide(index)
+ }
+
+ useEffect(() => {
+    if(props.auto){
+        const slideAuto = setInterval(() => {
+            nextSlider()
+        }, timeout);
+        return () => {
+            clearInterval(slideAuto)
+        }}},[nextSlider,timeout,props])
+
+
   console.log(data);
     return (
     <div className='hero-slider'>
@@ -19,16 +48,16 @@ const HeroSlider = props => {
       {
         props.control ? (
             <div className='hero-slider__control'>
-                <div className='hero-slider__control__item'>
-                    <i className='bx bx-chervon-left'></i>
+                <div className='hero-slider__control__item' onClick={prevSlider}>
+                <i className="bx bx-chevron-left"></i>
                 </div>
                 <div className='hero-slider__control__item'>
                     <div className="index">
                         {activeSlide + 1 } / {data.length}
                      </div>
                 </div>
-                <div className='hero-slider__control__item'>
-                    <i className='bx bx-chervon-right'></i>
+                <div className='hero-slider__control__item' onClick={nextSlider}>
+                <i className="bx bx-chevron-right"></i>
                 </div>
             </div>
         ) : null
@@ -39,7 +68,9 @@ const HeroSlider = props => {
 
 HeroSlider.propTypes = {
     data : PropTypes.array.isRequired,
-    control : PropTypes.bool
+    control : PropTypes.bool,
+    auto : PropTypes.bool,
+    timeout : PropTypes.number
 }
 
 const HeroSliderItem = props => (
@@ -53,7 +84,13 @@ const HeroSliderItem = props => (
             </div>
             <div className="hero-slider__item__info__btn">
                 <Link to={props.item.path}>
-                    <button>xem chi tiết</button>
+                    <Button
+                        backgroundColor={props.item.color}
+                        icon="bx bx-cart"
+                        animate={true}  
+                    >
+                        xem chi tiết
+                    </Button>
                 </Link>
             </div>
         </div>
