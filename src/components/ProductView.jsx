10c6be1c9c430dd9f5aa06f702ce4b831130f4a3
Nhat2807999 <nhat28071999@gmail.com>
2022-06-14 22:18:38 +0700

    done page

--- src/components/ProductView.jsx
+++ src/components/ProductView.jsx
@@ -1,14 +1,62 @@
-import React from 'react'
+import React , {useEffect} from 'react'
 import PropTypes from 'prop-types'
 import { useState } from 'react';
 import Button from "../components/Button"
 import numberWidthCommas from '../untils/numberWidthCommas';
+import { widthRouter} from 'react-router';
 const ProductView = props => {
   const product = props.product;
   const [previewImg,setPreviewImg] = useState(product.image01)
 
 
   const [descriptionExpand,setDescriptionExpand] = useState(false);
+
+  const [color,setColor] = useState(undefined);
+  const [size,setSize] = useState(undefined);
+  const [quanlity,setQuanlity] = useState(1); 
+
+  const updateQuanlity = (type) => {
+    if(type === 'plus'){
+        setQuanlity(quanlity + 1);
+    }else{
+        setQuanlity(quanlity - 1 < 1 ? 1 : quanlity - 1);
+    }
+  }
+
+  useEffect(() => {
+    setPreviewImg(product.image01)
+    setQuanlity(1)
+    setColor(undefined)
+    setSize(undefined)
+  },[product])
+
+  const check = () => {
+    let res = true;
+
+    if(color === undefined){
+        alert("Vui lòng chọn màu sắc");
+        return false;
+    }
+
+    if(size === undefined){
+        alert("Vui lòng chọn size");
+        return false;
+    }
+
+    return true;
+  }
+  
+  const addtoCart = () => {
+        if(check()){
+            alert(color);
+        }
+  }
+
+  const gotoCart = () => {
+    if(check()){
+        props.history.push('/cart');
+    }
+  }
 
   return (
     <div className="product">
@@ -57,8 +105,12 @@ const ProductView = props => {
                     {
                         product.colors.map((item,index) => {
                             return (
-                                <div key={index} className="product__info__item__list__item">
-                                        {item}
+                                <div 
+                                key={index} 
+                                className={`product__info__item__list__item ${color === item ? 'active' : ''}`}
+                                onClick = {() => setColor(item  )}
+                                >
+                                    <div className={`circle bg-${item}`}></div>
                                 </div>
                             )
                         })
@@ -73,13 +125,43 @@ const ProductView = props => {
                     {
                         product.size.map((item,index) => {
                             return (
-                                <div key={index} className="product__info__item__list__item">
+                                <div 
+                                key={index} 
+                                className={`product__info__item__list__item ${size === item ? 'active' : '' }`}
+                                onClick = {() => setSize(item)}
+                                >
+                                <span key={index} className="product__info__item__list__item_size">    
                                         {item}
+                                </span>
                                 </div>
                             )
                         })
                     }
                 </div>
+                </div>
+                <div className="product__info__item">
+                    <div className="product__info__item__title">
+                        Số lượng 
+                    </div>
+                <div className="product__info__item__quanlity">
+                    <div className='product__info__item__quanlity__btn'
+                    onClick = {() => updateQuanlity()}
+                    >
+                        <i className="bx bx-minus"></i>
+                    </div>
+                    <div className='product__info__item__quanlity__btn'>
+                        {quanlity}
+                    </div>
+                    <div className='product__info__item__quanlity__btn'
+                    onClick = {() => updateQuanlity("plus")}
+                    >
+                        <i className="bx bx-plus"></i>
+                    </div>
+                </div>
+                </div>
+                <div className="product__info__item">
+                    <Button onClick={() => addtoCart()}>Thêm vào giỏ</Button>
+                    <Button onClick={() => gotoCart()}>Mua ngay</Button>
                 </div>
          </div>
     </div>
@@ -90,4 +172,4 @@ ProductView.propTypes = {
     product : PropTypes.object.isRequired
 }
 
-export default ProductView
+export default widthRouter(ProductView)
